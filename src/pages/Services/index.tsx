d55386cc367460d63e20263902258db930a4c3c6
Saddam-tech <salokhiddinov0727@gmail.com>
2023-12-29 11:40:24 +0900

    added the fade effect to services page

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -4,6 +4,7 @@ import profileBackground from "assets/services-background.webp"
 import { socials } from 'data';
 import ServicesBlog from 'components/ServicesBlog';
 import QuoteInput from 'components/QuoteInput';
+import { Fade } from "react-awesome-reveal";
 const socialsConf = { fontSize: 40, color: "#ffffff" };
 const blogs = [{ h1: 'Exquisite Fleet of Luxury Buses', text: 'Our fleet boasts a collection of meticulously maintained luxury buses designed for comfort, elegance, and maximum enjoyment. From plush seating to cutting-edge amenities, we redefine the standards of luxury travel.', img: "services-0.webp" },
 { h1: 'Spacious Interiors for Group Comfort', text: 'With ample seating capacity, our buses provide a spacious and comfortable environment for your entire group. Enjoy the journey together in style, whether you`re planning a corporate outing or a family celebration.', img: "services-1.webp" },
@@ -13,44 +14,46 @@ const blogs = [{ h1: 'Exquisite Fleet of Luxury Buses', text: 'Our fleet boasts 
 const Services = () => {
     return (
         <Container>
-            {/* profile-wrap */}
-            <section className="profile-wrap">
-                <div className="backdrop"></div>
-                <div className="text-wrap">
-                    <h1>OUR SERVICES</h1>
-                    <p>Luxury Bus Charters: Elevate Your Events with Unparalleled Comfort and Style. <br />
-                        🌟 Welcome to Summit Charter Services Luxury Bus Charters – Where Every Journey Is a Grand Experience!
-                        Transform your events into unforgettable experiences with our opulent fleet of luxury buses. Whether it's a corporate gathering, wedding, sports event, or any special occasion, Summit Charter Services is your premier choice for unparalleled comfort and style.
-                    </p>
-                </div>
-                <div className="social-media-list">
-                    {socials.map((el, i) => (
-                        <a key={i} target="_blank" rel="noreferrer" href={el.path}>
-                            <el.icon sx={socialsConf} />
-                        </a>
+            <Fade cascade damping={0.1}>
+                {/* profile-wrap */}
+                <section className="profile-wrap">
+                    <div className="backdrop"></div>
+                    <div className="text-wrap">
+                        <h1>OUR SERVICES</h1>
+                        <p>Luxury Bus Charters: Elevate Your Events with Unparalleled Comfort and Style. <br />
+                            🌟 Welcome to Summit Charter Services Luxury Bus Charters – Where Every Journey Is a Grand Experience!
+                            Transform your events into unforgettable experiences with our opulent fleet of luxury buses. Whether it's a corporate gathering, wedding, sports event, or any special occasion, Summit Charter Services is your premier choice for unparalleled comfort and style.
+                        </p>
+                    </div>
+                    <div className="social-media-list">
+                        {socials.map((el, i) => (
+                            <a key={i} target="_blank" rel="noreferrer" href={el.path}>
+                                <el.icon sx={socialsConf} />
+                            </a>
+                        ))}
+                    </div>
+                </section>
+                {/* profile-wrap */}
+
+                {/* strip map */}
+                <section className="strip-map">
+                    <ul>
+                        <li>Corporate Services</li>
+                        <li>Charters & Tours</li>
+                        <li>Events & Entertainment</li>
+                        <li>Custom solutions</li>
+                    </ul>
+                </section>
+                {/* strip map */}
+
+                {/* blogs-holder */}
+                <section className="blogs-holder">
+                    {blogs.map((el, i) => (
+                        <ServicesBlog key={i} h1={el.h1} text={el.text} img={el.img} />
                     ))}
-                </div>
-            </section>
-            {/* profile-wrap */}
-
-            {/* strip map */}
-            <section className="strip-map">
-                <ul>
-                    <li>Corporate Services</li>
-                    <li>Charters & Tours</li>
-                    <li>Events & Entertainment</li>
-                    <li>Custom solutions</li>
-                </ul>
-            </section>
-            {/* strip map */}
-
-            {/* blogs-holder */}
-            <section className="blogs-holder">
-                {blogs.map((el, i) => (
-                    <ServicesBlog key={i} h1={el.h1} text={el.text} img={el.img} />
-                ))}
-            </section>
-            {/* blogs-holder */}
+                </section>
+                {/* blogs-holder */}
+            </Fade>
         </Container>
     )
 }
